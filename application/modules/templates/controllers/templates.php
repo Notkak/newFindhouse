aa0589e6b489860db501f29da07c78ea171254a9
SKYUSER-4D008AE\win10 <Notkak1994@gmail.com>
2017-01-13 19:18:11 +0700

    day 1

---
+++ application/modules/templates/controllers/templates.php
@@ -0,0 +1,20 @@
+<?php
+class templates extends MX_Controller {
+
+  function __construct() {
+    parent::__construct();
+  }
+  function test(){
+    $data="";
+    $this->admin($data);
+  }
+  function public_bootstrap($data){
+    $this->load->view('public_bootstrap', $data);
+  }
+  function public_jqm($data){
+    $this->load->view('public_jqm', $data);
+  }
+  function admin($data){
+    $this->load->view('admin', $data);
+  }
+}
